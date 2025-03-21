1e60dacf4daa400c28169fb4ee7bdb952f8147cf
BorisHandjiev <terzeevkoko@gmail.com>
2025-03-21 22:13:13 +0200

    pushing touch sensor

--- BackErecter2000/BackErecter2000.ino
+++ BackErecter2000/BackErecter2000.ino
@@ -8,12 +8,12 @@ int state;
 int angle(int16_t &gyroX, int16_t &gyroY, int16_t &gyroZ) {  // Pass by reference
     if (mywire.available() == 6) {  // Use 'mywire', not 'myWire'
         gyroX = mywire.read() | (mywire.read() << 8);
-        gyroY = mywire.read() | (mywire.read() << 8);
+      //  gyroY = mywire.read() | (mywire.read() << 8);
         gyroZ = mywire.read() | (mywire.read() << 8);
     }
 
     float angleX = gyroX * 0.00875;
-    float angleY = gyroY * 0.00875;
+   // float angleY = gyroY * 0.00875;
     float angleZ = gyroZ * 0.00875;
 
     for(int i=1;i<=10;i++)
@@ -26,14 +26,14 @@ int angle(int16_t &gyroX, int16_t &gyroY, int16_t &gyroZ) {  // Pass by referenc
       if (mywire.available() == 6) 
       {
         gyroX = mywire.read() | (mywire.read() << 8);
-        gyroY = mywire.read() | (mywire.read() << 8);
+        //gyroY = mywire.read() | (mywire.read() << 8);
         gyroZ = mywire.read() | (mywire.read() << 8);
       }
 
       float angleX = gyroX * 0.00875;
-      float angleY = gyroY * 0.00875;
+     // float angleY = gyroY * 0.00875;
       float angleZ = gyroZ * 0.00875;
-        if (angleX <30) 
+        if (angleX <10 && (angleZ<-10 || angleZ>10)) 
           {
             return 0;
           }
@@ -53,14 +53,14 @@ void setup() {
 }
 
 void loop() {
-    int16_t gyroX, gyroY, gyroZ;
+    int16_t gyroX, /*gyroY, */gyroZ;
     state=digitalRead(touch_pin);
     if(state==HIGH){
       mywire.beginTransmission(K2230_ADDR);
       mywire.write(0x28 | 0x80);
       mywire.endTransmission();
       mywire.requestFrom(K2230_ADDR, 6);
-      while (angle(gyroX, gyroY, gyroZ) == 0) {
+      while (angle(gyroX, /*gyroY,*/ gyroZ) == 0) {
           tone(Buzzer, 300);
           delay(500);
           angle(gyroX, gyroY, gyroZ); 
