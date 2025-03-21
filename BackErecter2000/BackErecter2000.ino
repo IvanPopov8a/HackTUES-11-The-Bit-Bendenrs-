b8b2f48499600c4a14ef6c2c4e54a5c824aab554
BorisHandjiev <terzeevkoko@gmail.com>
2025-03-22 01:54:24 +0200

    removing comments

--- BackErecter2000/BackErecter2000.ino
+++ BackErecter2000/BackErecter2000.ino
@@ -1,19 +1,17 @@
 #include <SoftWire.h>
 
 #define K2230_ADDR 0x68
-SoftWire mywire(2, 3); // Ensure to use the same variable name as defined above
+SoftWire mywire(2, 3);
 int Buzzer = 12;
 int touch_pin=5;
 int state;
-int angle(int16_t &gyroX, int16_t &gyroY, int16_t &gyroZ) {  // Pass by reference
-    if (mywire.available() == 6) {  // Use 'mywire', not 'myWire'
+int angle(int16_t &gyroX, int16_t &gyroY, int16_t &gyroZ) {
+    if (mywire.available() == 6) {
         gyroX = mywire.read() | (mywire.read() << 8);
-      //  gyroY = mywire.read() | (mywire.read() << 8);
         gyroZ = mywire.read() | (mywire.read() << 8);
     }
 
     float angleX = gyroX * 0.00875;
-   // float angleY = gyroY * 0.00875;
     float angleZ = gyroZ * 0.00875;
 
     for(int i=1;i<=10;i++)
@@ -26,12 +24,10 @@ int angle(int16_t &gyroX, int16_t &gyroY, int16_t &gyroZ) {  // Pass by referenc
       if (mywire.available() == 6) 
       {
         gyroX = mywire.read() | (mywire.read() << 8);
-        //gyroY = mywire.read() | (mywire.read() << 8);
         gyroZ = mywire.read() | (mywire.read() << 8);
       }
 
       float angleX = gyroX * 0.00875;
-     // float angleY = gyroY * 0.00875;
       float angleZ = gyroZ * 0.00875;
         if (angleX <10 && (angleZ<-10 || angleZ>10)) 
           {
@@ -43,7 +39,7 @@ int angle(int16_t &gyroX, int16_t &gyroY, int16_t &gyroZ) {  // Pass by referenc
 
 void setup() {
     pinMode(touch_pin, INPUT);
-    mywire.begin();  // Correct the usage to 'mywire'
+    mywire.begin();
     mywire.beginTransmission(K2230_ADDR);
     mywire.write(0x20);
     mywire.write(0x0F);
@@ -53,14 +49,14 @@ void setup() {
 }
 
 void loop() {
-    int16_t gyroX, /*gyroY, */gyroZ;
+    int16_t gyroX,gyroZ;
     state=digitalRead(touch_pin);
     if(state==HIGH){
       mywire.beginTransmission(K2230_ADDR);
       mywire.write(0x28 | 0x80);
       mywire.endTransmission();
       mywire.requestFrom(K2230_ADDR, 6);
-      while (angle(gyroX, /*gyroY,*/ gyroZ) == 0) {
+      while (angle(gyroX,gyroZ) == 0) {
           tone(Buzzer, 300);
           delay(500);
           angle(gyroX, gyroY, gyroZ); 
